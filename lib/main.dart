d032bdfc45f0c0c61b137e3a4a35307d7a0c66df
hirajya <rodneyestrada2425@gmail.com>
2024-03-18 15:30:44 +0800

    .

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ void main() {
 }
 
 class MyApp extends StatefulWidget {
-  MyApp({super.key});
+  MyApp({Key? key}) : super(key: key);
 
   @override
   State<MyApp> createState() => _MyAppState();
@@ -19,50 +19,115 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('App Title'),
-          backgroundColor: Colors.blue,
+      home: FirstPage(
+        currentIndex: currentIndex,
+        onButtonPressed: () {
+          setState(() {
+            buttonName = 'Clicked';
+          });
+        },
+        onBottomNavBarTap: (int index) {
+          setState(() {
+            currentIndex = index;
+          });
+        },
+      ),
+    );
+  }
+}
+
+class FirstPage extends StatelessWidget {
+  final int currentIndex;
+  final VoidCallback onButtonPressed;
+  final ValueChanged<int> onBottomNavBarTap;
+
+  const FirstPage({
+    required this.currentIndex,
+    required this.onButtonPressed,
+    required this.onBottomNavBarTap,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'App Title',
+          style: TextStyle(color: Colors.white),
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton(
-                onPressed: () {
-                  setState(() {
-                    buttonName = 'Clicked';
-                  });
-                },
-                child: Text(buttonName),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  setState(() {
-                    buttonName = 'Clicked';
-                  });
-                },
-                child: Text(buttonName),
-              ),
-            ],
+        backgroundColor: Colors.black,
+      ),
+      body: Center(
+        child: currentIndex == 0
+            ? Container(
+                height: double.infinity,
+                width: double.infinity,
+                color: Colors.white,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        foregroundColor: Colors.white,
+                        backgroundColor: Colors.black,
+                      ),
+                      onPressed: onButtonPressed,
+                      child: Text('Click'),
+                    ),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        foregroundColor: Colors.black,
+                        backgroundColor: Colors.white,
+                      ),
+                      onPressed: () {
+                        onButtonPressed();
+                        Navigator.of(context).push(MaterialPageRoute(
+                            builder: (BuildContext context) {
+                          return const SecondPage();
+                        }));
+                      },
+                      child: Text('Click'),
+                    ),
+                  ],
+                ),
+              )
+            : Image.asset('images/sadpepe.jpg'),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
           ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+        ],
+        currentIndex: currentIndex,
+        onTap: onBottomNavBarTap,
+      ),
+    );
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  const SecondPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Second Page',
+          style: TextStyle(color: Colors.white),
         ),
-        bottomNavigationBar: BottomNavigationBar(
-          items: const [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: 'Home',
-            ),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.settings), label: 'Settings'),
-          ],
-          currentIndex: currentIndex,
-          onTap: (int index) {
-            setState(() {
-              currentIndex = index;
-            });
-          },
-        ),
+        backgroundColor: Colors.black,
+      ),
+      body: Center(
+        child: Text('Second Page'),
       ),
     );
   }
--- lib/sample.dart
+++ lib/sample.dart
@@ -5,86 +5,129 @@ void main() {
 }
 
 class MyApp extends StatefulWidget {
-  const MyApp({super.key});
+  MyApp({Key? key}) : super(key: key);
 
   @override
   State<MyApp> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-  int count = 0;
-
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-  }
+  String buttonName = 'Click';
+  int currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.blue,
-          title: const Text('Home'),
-        ),
+      debugShowCheckedModeBanner: false,
+      home: FirstPage(
+        currentIndex: currentIndex,
+        onButtonPressed: () {
+          setState(() {
+            buttonName = 'Clicked';
+          });
+        },
+        onBottomNavBarTap: (int index) {
+          setState(() {
+            currentIndex = index;
+          });
+        },
+      ),
+    );
+  }
+}
 
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            setState(() {
-              count++;
-            });
-          },
-          child: Icon(Icons.add),
-          backgroundColor: Colors.blue,
-        ),
+class FirstPage extends StatelessWidget {
+  final int currentIndex;
+  final VoidCallback onButtonPressed;
+  final ValueChanged<int> onBottomNavBarTap;
 
-        body: Center(
-          child: Text(
-            '$count',
-            style: const TextStyle(fontSize: 60),
-          ),
-        ),
+  const FirstPage({
+    required this.currentIndex,
+    required this.onButtonPressed,
+    required this.onBottomNavBarTap,
+    Key? key,
+  }) : super(key: key);
 
-        bottomNavigationBar: BottomNavigationBar(items: const [
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'App Title',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.black,
+      ),
+      body: Center(
+        child: currentIndex == 0
+            ? Container(
+                height: double.infinity,
+                width: double.infinity,
+                color: Colors.white,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        foregroundColor: Colors.white,
+                        backgroundColor: Colors.black,
+                      ),
+                      onPressed: onButtonPressed,
+                      child: Text('Click'),
+                    ),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        foregroundColor: Colors.black,
+                        backgroundColor: Colors.white,
+                      ),
+                      onPressed: () {
+                        onButtonPressed();
+                        Navigator.of(context).push(MaterialPageRoute(
+                            builder: (BuildContext context) {
+                          return const SecondPage();
+                        }));
+                      },
+                      child: Text('Click'),
+                    ),
+                  ],
+                ),
+              )
+            : Image.asset('images/sadpepe.jpg'),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
           BottomNavigationBarItem(
             icon: Icon(Icons.home),
             label: 'Home',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.business),
-            label: 'Business',
+            icon: Icon(Icons.settings),
+            label: 'Settings',
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.school),
-            label: 'School',
-          ),
-        ]),
+        ],
+        currentIndex: currentIndex,
+        onTap: onBottomNavBarTap,
+      ),
+    );
+  }
+}
 
-        drawer: const Drawer(child: Text('Yo!')),
+class SecondPage extends StatelessWidget {
+  const SecondPage({Key? key}) : super(key: key);
 
-        // body: ListView(
-        //   scrollDirection: Axis.horizontal,
-        //   addAutomaticKeepAlives: false,
-        //   children: [
-        //     Container(
-        //       width: 400,
-        //       color: Colors.red,
-        //     ),
-        //     Container(
-        //       width: 400,
-        //       color: Colors.blue,
-        //     ),
-        //     Container(
-        //       width: 400,
-        //       color: Colors.green,
-        //     ),
-        //   ]
-        //   )
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Second Page',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.black,
+      ),
+      body: Center(
+        child: Text('Second Page'),
       ),
     );
   }
