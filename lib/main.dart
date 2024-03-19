323a98d7fe169f9423f30c995a6dc5623bd1e70d
hirajya <rodneyestrada2425@gmail.com>
2024-03-19 20:49:45 +0800

    goods

--- lib/main.dart
+++ lib/main.dart
@@ -1,173 +1,261 @@
+import 'dart:ui';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatefulWidget {
-  MyApp({Key? key}) : super(key: key);
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  String buttonName = 'Click';
-  int currentIndex = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: FirstPage(
-        currentIndex: currentIndex,
-        onButtonPressed: () {
-          setState(() {
-            buttonName = 'Clicked';
-          });
-        },
-        onBottomNavBarTap: (int index) {
-          setState(() {
-            currentIndex = index;
-          });
-        },
+void main() => runApp(
+      const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: HomePage(),
       ),
     );
-  }
-}
 
-class FirstPage extends StatefulWidget {
-  final int currentIndex;
-  final VoidCallback onButtonPressed;
-  final ValueChanged<int> onBottomNavBarTap;
-
-  const FirstPage({
-    required this.currentIndex,
-    required this.onButtonPressed,
-    required this.onBottomNavBarTap,
-    Key? key,
-  }) : super(key: key);
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
-  _FirstPageState createState() => _FirstPageState();
+  _HomePageState createState() => _HomePageState();
 }
 
-class _FirstPageState extends State<FirstPage> {
-  bool showImage = false;
+class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _animation;
+
+  TextEditingController _emailController = TextEditingController(); // Controller for email TextField
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+    _animation = Tween<double>(begin: 0.0, end: 1.0).animate(_animationController);
+    _animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    _emailController.dispose(); // Dispose the controller
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'App Title',
-          style: TextStyle(color: Colors.white),
+      body: Container(
+        width: double.infinity,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color(0xFF0D47A1), // Dark blue
+              Color(0xFF1565C0), // Slightly lighter dark blue
+              Color(0xFF1E88E5), // Lighter blue
+            ],
+          ),
         ),
-        backgroundColor: Colors.black,
-      ),
-      body: Center(
-        child: widget.currentIndex == 0
-            ? showImage
-                ? GestureDetector(
-                    onTap: () {
-                      setState(() {
-                        showImage = false;
-                      });
-                    },
-                    child: Image.asset('images/sadpepe.jpg'),
-                  )
-                : Container(
-                    height: double.infinity,
-                    width: double.infinity,
-                    color: Colors.white,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            foregroundColor: Colors.white,
-                            backgroundColor: Colors.black,
-                          ),
-                          onPressed: widget.onButtonPressed,
-                          child: Text('Click'),
-                        ),
-                        ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            foregroundColor: Colors.black,
-                            backgroundColor: Colors.white,
-                          ),
-                          onPressed: () {
-                            widget.onButtonPressed();
-                            Navigator.of(context).push(
-                              MaterialPageRoute(builder: (BuildContext context) {
-                                return const SecondPage();
-                              }),
-                            );
-                          },
-                          child: Text('Click'),
-                        ),
-                      ],
-                    ),
-                  )
-            : GestureDetector(
-                onTap: () {
-                  setState(() {
-                    showImage = !showImage;
-                  });
-                },
-                child: showImage
-                    ? Image.asset('images/sadpepe.jpg')
-                    : Image.asset('images/whatcat.png'),
+        child: SingleChildScrollView(
+          // Wrap with SingleChildScrollView
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              const SizedBox(
+                height: 80,
               ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
+              Padding(
+                padding: const EdgeInsets.all(20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    FadeTransition(
+                      opacity: _animation,
+                      child: Text(
+                        "Login",
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 40,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      "Welcome Back",
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Container(
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(60),
+                      topRight: Radius.circular(60)),
+                ),
+                child: Padding(
+                  padding: EdgeInsets.all(30),
+                  child: Column(
+                    children: <Widget>[
+                      const SizedBox(
+                        height: 60,
+                      ),
+                      Container(
+                        padding: const EdgeInsets.all(20),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10),
+                          boxShadow: const [
+                            BoxShadow(
+                              color: Colors.grey,
+                              blurRadius: 20,
+                              offset: Offset(0, 10),
+                            ),
+                          ],
+                        ),
+                        child: Column(
+                          children: <Widget>[
+                            Container(
+                              decoration: BoxDecoration(
+                                  border: Border(
+                                      bottom: BorderSide(
+                                          color: Colors.grey[200]!))),
+                              child: TextField(
+                                controller: _emailController, // Assign the controller
+                                decoration: const InputDecoration(
+                                  hintText: "Email",
+                                  hintStyle: TextStyle(color: Colors.grey),
+                                  border: InputBorder.none,
+                                ),
+                              ),
+                            ),
+                            Container(
+                              decoration: BoxDecoration(
+                                  border: Border(
+                                      bottom: BorderSide(
+                                          color: Colors.grey[200]!))),
+                              child: const TextField(
+                                decoration: InputDecoration(
+                                  hintText: "Password",
+                                  hintStyle: TextStyle(color: Colors.grey),
+                                  border: InputBorder.none,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 40,
+                      ),
+                      const Text(
+                        "Forgot Password?",
+                        style: TextStyle(
+                          color: Colors.grey,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          // Navigate to the second screen with email value
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => SecondPage(email: _emailController.text)),
+                          );
+                        },
+                        child: const Text('Login'),
+                      ),
+                      const SizedBox(
+                        height: 40,
+                      ),
+                      const Text(
+                        "Continue with social media",
+                        style: TextStyle(
+                          color: Colors.grey,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Row(
+                        children: <Widget>[
+                          Expanded(
+                            child: Container(
+                              height: 50,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(50),
+                                color: Colors.blue,
+                              ),
+                              child: const Center(
+                                child: Text(
+                                  "Facebook",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 30,
+                          ),
+                          Expanded(
+                            child: Container(
+                              height: 50,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(50),
+                                color: Colors.black,
+                              ),
+                              child: const Center(
+                                child: Text(
+                                  "Github",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.settings),
-            label: 'Settings',
-          ),
-        ],
-        currentIndex: widget.currentIndex,
-        onTap: widget.onBottomNavBarTap,
+        ),
       ),
     );
   }
 }
-
 
 class SecondPage extends StatelessWidget {
-  const SecondPage({Key? key}) : super(key: key);
+  final String email;
+
+  const SecondPage({Key? key, required this.email}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(
-          'Second Page',
-          style: TextStyle(color: Colors.white),
+        title: const Text('Second Page'),
+      ),
+      body: Center(
+        child: Text(
+          'Email: $email',
+          style: const TextStyle(fontSize: 20),
         ),
-        backgroundColor: Colors.black,
-      ),
-      body: const Center(
-        child: Text('Second Page'),
-      ),
-    );
-  }
-}
-
-class NextPage extends StatelessWidget {
-  const NextPage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Next Page'),
       ),
     );
   }
